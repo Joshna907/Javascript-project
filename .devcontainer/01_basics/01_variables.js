8a9987facf23d64f46851a12d058105bed22d467
Joshna Waikar <joshnawaikar@gmail.com>
2024-01-12 11:05:09 +0000

    d

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,7 +1,11 @@
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur"  //never use this type always use let or const
+let accountState ; //undefined - output 
+
+/* Prefer not to use var  because of issue in block scope and functional scope
+*/
 
 
 accountEmail = "hfc@google.com"
