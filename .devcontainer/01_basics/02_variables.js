3f8b292b21251082f19058a54040f292b6afffd9
Joshna Waikar <joshnawaikar@gmail.com>
2024-01-12 11:30:53 +0000

    datatypes

--- .devcontainer/01_basics/02_variables.js
+++ .devcontainer/01_basics/02_variables.js
@@ -1,2 +1,22 @@
-"use Strict" //treat all JS code as newer version
-alert( 3 + 3 )
+"use strict"; //treat all JS code as newer version
+//alert( 3 + 3 )// we are using node.js not browser
+
+let name = "hitesh";
+let number = 18
+let isLoggedin = false
+let state = null
+
+// number => 2 to power 53
+//bigInt => stockmarket, trading
+//string =>  ""
+//boolean => true/false
+//null => standalone value
+//undefined => value assign nahi hui hai
+//symbol => unique 
+
+
+//object 
+
+console.log(typeof "hitesh") //string
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object                                                              
