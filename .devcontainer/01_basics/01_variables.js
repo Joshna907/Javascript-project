6d43a08c6b684730a39fe00cd9a0965a74683efb
Joshna Waikar <joshnawaikar@gmail.com>
2024-01-12 10:53:16 +0000

    d

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,3 +1,12 @@
 const accountId = 144553
-accountId = 123456
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountEmail = "hfc@google.com"
+accountPassword = "34567"
+accountCity = "london"
+console.table([accountEmail, accountPassword, accountCity])
+//accountId = 123456  //not allowed
 console.log(accountId);
