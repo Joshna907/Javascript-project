52e655ae897710124a15ae4bc4a44a5504d7401c
Joshna Waikar <joshnawaikar@gmail.com>
2024-01-12 14:40:21 +0000

    conversion

--- .devcontainer/01_basics/03_conversion.js
+++ .devcontainer/01_basics/03_conversion.js
@@ -31,3 +31,45 @@ console.log(booleanIsLoggedIn);
 let Stringnumber = 78
 let Stringno = String(Stringnumber) // while converting the datatypes the datatype first letter is capital
 console.log(Stringno);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+//console.log(+true);
+//console.log(true+);      // not doneee
+//console.log(+"");
+
+//let num1, num2, num3
+
+//num1 = num2 = num3 = 2 + 2 
+  
+let gameCounter = 100
+ gameCounter++
+console.log(gameCounter);
