b5696de8b25919b1bfd0d5e67989b12594b574a9
Joshna Waikar <joshnawaikar@gmail.com>
2024-01-17 14:30:35 +0000

    scopes

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -1,12 +1,12 @@
 //var c = 300
-let a = 300
-if (true) {
-    let a = 10 // also not let they are available
-    const b = 20 // but not const
-    var c = 1 // you will see var is available as globally 
-    // console.log("INNER: ", a);
+// let a = 300
+// if (true) {
+//     let a = 10 // also not let they are available
+//     const b = 20 // but not const
+//     var c = 1 // you will see var is available as globally 
+//     // console.log("INNER: ", a);
 
-}
+// }
 
 // Good interview question, is that global scope is different in console and in coding environment
 
@@ -14,26 +14,39 @@ if (true) {
 // console.log(b);
  //console.log(c);
  
- function one(){                 
-    const username = "hitesh"         // the bigger functions variables are accepted in smaller fun
+//  function one(){                 
+//     const username = "hitesh"         // the bigger functions variables are accepted in smaller fun
 
-    function two(){
-        const website = "youtube"  // but the smaller function variables `cant give bigger function access
-        console.log(username);
-    }
-    // console.log(website);
+//     function two(){
+//         const website = "youtube"  // but the smaller function variables `cant give bigger function access
+//         console.log(username);
+//     }
+//     // console.log(website);
 
-     two()
+//      two()
 
-}
+// }
   // in short parents can give you icecream but we cant buy them icecream when we are small 
 // one()
 
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
+// if (true) {
+//     const username = "hitesh"
+//     if (username === "hitesh") {
+//         const website = " youtube"
+//         // console.log(username + website);
+//     }
 
     // ++++++++++++++++++ interesting ++++++++++++++++++
+
+//     console.log(addone(5)) // here we can do before
+
+// function addone(num){
+//     return num + 1
+// }
+
+
+
+// addTwo(5) // here we cant the function is not only declared but also stored
+// const addTwo = function(num){
+//     return num + 2
+// }
