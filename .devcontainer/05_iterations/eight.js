683df2169b1ce6c9afdd1df975a1d38cee703d42
Joshna Waikar <joshnawaikar@gmail.com>
2024-01-18 15:29:06 +0000

    reduce

---
+++ .devcontainer/05_iterations/eight.js
@@ -0,0 +1,35 @@
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce(function (acc, currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval                
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+ // accumulator is the initial value basically initially and addition with current value the next value and the output is added again as accumulator
+ 
+console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
