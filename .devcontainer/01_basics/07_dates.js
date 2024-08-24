17ad17c26fe0b19969e27f68f18b81aff674f2d6
Joshna Waikar <joshnawaikar@gmail.com>
2024-08-24 15:02:34 +0000

    d

--- .devcontainer/01_basics/07_dates.js
+++ .devcontainer/01_basics/07_dates.js
@@ -1,28 +1,28 @@
 let myDate = new Date()      // Date is an object
              //This all are methods
 //console.log(myDate);    
-//console.log(myDate.toLocaleString()); 
-//console.log(myDate.toJSON());  
+console.log(myDate.toLocaleString()); 
+// console.log(myDate.toJSON());  
  //console.log(myDate.toString());
  //console.log(myDate.toDateString());
  //console.log(typeof myDate); 
  
-  let myCreatedDate = new Date(2023, 6, 23)
+//  let myCreatedDate = new Date(2023, 6, 23)
  //let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 //let myCreatedDate = new Date("01-14-2023")
 //console.log(myCreatedDate.toLocaleString());
 
-let myTimeStamp = Date.now
+// let myTimeStamp = Date.now
 
 // console.log(myTimeStamp); // The value comes in milliseconds
  //console.log(myCreatedDate.getTime()); We will get time 
 // console.log(Math.floor(Date.now()/1000)); // we will get the milliseconds converted to seconds due to 1000 and round value due to Math.floor
 
-let newDa = new Date()
-console.log(newDate);
-console.log(newDa.getMonth() + 1);
-console.log(newDa.getDay);
+// let newDa = new Date()
+// console.log(newDate);
+// console.log(newDa.getMonth() + 1);
+// console.log(newDa.getDay);
 
 
 
